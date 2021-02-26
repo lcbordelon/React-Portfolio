d582fdcc373d24163761fcb17255dc1af7642aaa
Lindsay Bordelon <lcbordelon@gmail.com>
2021-02-26 13:26:02 -0700

    updated about me card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,10 +6,12 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    minHeight: 50,
   },
   bullet: {
     display: "inline-block",
@@ -23,8 +25,8 @@ const useStyles = makeStyles({
     marginBottom: 12,
   },
   media: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
+    height: 200,
+    width: 200,
   },
 });
 
@@ -33,35 +35,46 @@ export default function SimpleCard(props) {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          {props.title}
-        </Typography>
-        <CardMedia
-          className={classes.media}
-          image={props.image}
-          title={props.imagetitle}
-        />
-        <Typography variant="h5" component="h2">
-          {props.description}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={3}>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              {props.title}
+            </Typography>
+            <CardMedia
+              className={classes.media}
+              image={props.image}
+              title={props.imagetitle}
+            />
+            <Typography variant="h5" component="h2">
+              {props.description}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,17 @@
 import React from "react";
 import SimpleCard from "../components/Card/Card";
-import "../components/imgs/LB.PNG";
+import myPicture from "../components/imgs/LB.PNG";
+import "../components/styles.css";
+
 
 function About() {
+
   return (
     <div className="container">
       <SimpleCard
         title="Hello! I'm Lindsay!"
         description="blah blah blah react"
-        image="../components/imgs/LB.PNG"
+        image={myPicture}
         imagetitle="Lindsay Bordelon"
       />
     </div>
