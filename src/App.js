c8b5fc7bed7f08350c597c6c2143b71d3b5e5f7d
Lindsay Bordelon <lcbordelon@gmail.com>
2021-03-06 13:29:57 -0700

    700th push that probably won't work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -29,7 +24,6 @@ function App() {
             <Route exact path="/" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/portfolio" component={Portfolio} />
-            <Redirect from="/React-Portfolio" to="/" />
           </Switch>
           <Footer />
         </div>
