60a3fc726341b6cb645b491a8742f4ea8212f7b9
Lindsay Bordelon <lcbordelon@gmail.com>
2021-03-06 11:54:40 -0700

    deploy3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -8,7 +8,7 @@ import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <div
         style={{
           backgroundImage: `url("https://images.unsplash.com/photo-1503435980610-a51f3ddfee50?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDIxMXxibzhqUUtUYUUwWXx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")`,
@@ -21,15 +21,13 @@ function App() {
       >
         <Navbar />
         <Wrapper>
-          <Switch>
-            <Route exact path="/" component={About} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
-          </Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
         <Footer />
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
